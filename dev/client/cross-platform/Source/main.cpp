e70e7ef60b03c54fdf4e94dd38020ecdd6f02d4b
teamaffixmb-jake <66917299+teamaffixmb-jake@users.noreply.github.com>
2020-07-26 14:34:03 -0700

    Added CP Test Code

--- dev/client/cross-platform/Source/main.cpp
+++ dev/client/cross-platform/Source/main.cpp
@@ -1,11 +1,13 @@
 #pragma once
 #include "../../cross-platform/header/superHeader.h"
 #include "../../cross-platform/header/startupwindow.h"
+#include "../../cross-platform/header/convert.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     StartUpWindow w;
+    string od = getOSDir("test\\123");
     w.show();
     return a.exec();
 }
