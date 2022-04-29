726ebb4ae816b7a8008ef360d434fc929eb4b192
Jake Harrison <teamaffixmb.jake@gmail.com>
2022-04-29 14:35:10 -0700

    Updated agent class

--- affix-services/affix-services/agent.h
+++ affix-services/affix-services/agent.h
@@ -14,11 +14,6 @@ namespace affix_services
 		std::string m_type_identifier;
 
 		/// <summary>
-		/// Additional information for any remote client or agent curious.
-		/// </summary>
-		std::vector<uint8_t> m_agent_specific_information;
-
-		/// <summary>
 		/// Timestamp for the agent object, which is measured in seconds since January 1, 1970.
 		/// </summary>
 		uint64_t m_timestamp = 0;
@@ -47,16 +42,43 @@ namespace affix_services
 		/// <param name="a_agent_specific_information"></param>
 		agent(
 			affix_services::client& a_local_client,
-			const std::string& a_type_identifier,
-			const std::vector<uint8_t>& a_agent_specific_information = {}
+			const std::string& a_type_identifier
 		);
 
 		/// <summary>
 		/// Discloses the agent information using the client.
 		/// </summary>
+		template<typename AGENT_SPECIFIC_INFORMATION_TYPE>
 		void disclose_agent_information(
+			const AGENT_SPECIFIC_INFORMATION_TYPE& a_agent_specific_information
+		)
+		{
+			// Lock the vector of agent information messages
+			locked_resource l_agent_information_messages = m_local_client.m_agent_information_messages.lock();
 
-		);
+			// Serialize the agent_specific_information
+			affix_base::data::byte_buffer l_agent_specific_information_byte_buffer;
+			l_agent_specific_information_byte_buffer.push_back(a_agent_specific_information);
+
+			// Create the message body
+			message_agent_information_body l_message_body(
+				m_local_client.m_local_identity,
+				agent_information(
+					m_type_identifier,
+					l_agent_specific_information_byte_buffer.data(),
+					m_timestamp,
+					m_disclosure_iteration));
+
+			// Create the message
+			message l_message(l_message_body.create_message_header(), l_message_body);
+
+			// Add the message to the vector
+			l_agent_information_messages->push_back(l_message);
+
+			// Increment the disclosure iteration.
+			m_disclosure_iteration++;
+
+		}
 
 	};
 }
